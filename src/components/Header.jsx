7574ebf11cac4361790ef015c564f1b65dac4602
MattiaPlacido <placido.mattia@gmail.com>
2024-12-16 12:22:04 +0100

    Header con input reattivo

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,30 @@
+import { useState } from "react";
+
 export default function Header() {
+  const [queryTerm, setQueryTerm] = useState("");
+
+  const handleInputChange = (e) => {
+    setQueryTerm(e.target.value);
+  };
+
   return (
-    <div className="bg-dark text-danger p-3">
+    <div className="bg-dark text-danger py-3 d-flex justify-content-between px-5">
       <h1> BoolFlix </h1>
+      <form>
+        <div className="d-flex ">
+          <input
+            type="text"
+            className="form-control"
+            id="film-query-input"
+            name="queryTerm"
+            value={queryTerm}
+            onChange={handleInputChange}
+          />
+          <button type="submit" className="btn btn-danger">
+            Cerca
+          </button>
+        </div>
+      </form>
     </div>
   );
 }
