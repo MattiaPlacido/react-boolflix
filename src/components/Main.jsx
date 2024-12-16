98d3170bc226c84cc3aee7934593cf2d9bb1b83b
MattiaPlacido <placido.mattia@gmail.com>
2024-12-16 13:10:52 +0100

    Milestone 1 completa

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -6,8 +6,22 @@ export default function Main() {
   return (
     <div className="bg-light text-dark p-5">
       <h1>{currentMovies.length > 0 ? "Risultati :" : "Cerca Qualcosa!"}</h1>
-      <div>
-        {currentMovies}
+      <div className={`container ${currentMovies.length > 0 ? "" : "d-none"}`}>
+        <ul>
+          {currentMovies.map((movie) => {
+            return (
+              <li className="mb-3">
+                Titolo : <b>{movie.title}</b>
+                <br />
+                Titolo originale : <b>{movie.original_title}</b>
+                <br />
+                Lingua originale : <b>{movie.original_language}</b>
+                <br />
+                Voto medio dalla comunità : <b>{movie.vote_average}</b>
+              </li>
+            );
+          })}
+        </ul>
       </div>
     </div>
   );
